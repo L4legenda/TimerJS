224615656a8de9dbb27fb22a921c105bf06a7ead
l4legenda <l4legenda@gmail.com>
2020-12-09 19:57:35 +0500

    filter

--- js/script.js
+++ js/script.js
@@ -1,21 +1,68 @@
-let elements = document.querySelectorAll(".element");
+let data = [
+   {
+       name: "Filter C",
+       img: "https://s0.rbk.ru/v6_top_pics/ampresize/media/img/4/59/755958619082594.jpg",
+        text: "asdasd",
+        price: 250,
+        type: "c"
+    },
+    {
+        name: "Filter B",
+        img: "https://s0.rbk.ru/v6_top_pics/ampresize/media/img/4/59/755958619082594.jpg",
+         text: "asdasd",
+         price: 350,
+         type: "b"
+     },
+     {
+        name: "Filter A",
+        img: "https://s0.rbk.ru/v6_top_pics/ampresize/media/img/4/59/755958619082594.jpg",
+         text: "asdasd",
+         price: 450,
+         type: "a"
+     }
 
-let i = 0;
+]
 
-setInterval( () => {
-    if(i == 0){
-        for( let el of elements ){
-            el.style.opacity = 0;
+
+function filterA(){
+    reder("a")
+}
+
+function filterB(){
+    reder("b")
+}
+
+function filterC(){
+    reder("c")
+}
+
+function filterAll(){
+    reder("all")
+}
+
+
+
+function reder(filter){
+    let shop = document.querySelector(".shop");
+    shop.innerHTML = "";
+
+    for( let i in data ){
+        if(filter == data[i].type  || filter == "all"){
+            let tovar = document.createElement("div");
+        
+            let name = document.createElement("p");
+            name.innerHTML = data[i].name;
+            
+            let img = document.createElement("img");
+            img.src = data[i].img;
+    
+            tovar.append(name);
+            tovar.append(img);
+    
+            shop.append(tovar);
         }
+        
     }
-
-    elements[i].style.opacity = 1;
-    i = i + 1;
-    
-    if(i > 2){
-        i=0;
-    }
-    
-}, 3000 );
+}
 
 
