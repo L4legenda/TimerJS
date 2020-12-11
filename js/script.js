d014d779924b97d56128e3867fb5e184e47ecf3f
l4legenda <l4legenda@gmail.com>
2020-12-11 19:30:31 +0500

    navigate

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+let el = document.querySelector(".header nav");
+let btn = document.querySelector(".header button");
+let nav = 0;
 
-
-
+function showNav(){
+    if(nav == 0){
+        btn.innerHTML = "-";
+        el.style.display = "flex";
+        nav = 1;
+    }else if( nav == 1){
+        btn.innerHTML = "+";
+        el.style.display = "";
+        nav = 0;
+    }
+}
