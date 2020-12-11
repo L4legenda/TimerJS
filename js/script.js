ca482f7eb1487d03984f94a15c8302dddfe39709
l4legenda <l4legenda@gmail.com>
2020-12-11 19:11:56 +0500

    flex

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
-let elements = document.querySelectorAll(".element");
 
-let i = 0;
-
-setInterval( () => {
-    if(i == 0){
-        for( let el of elements ){
-            el.style.opacity = 0;
-        }
-    }
-
-    elements[i].style.opacity = 1;
-    i = i + 1;
-    
-    if(i > 2){
-        i=0;
-    }
-    
-}, 3000 );
 
 
